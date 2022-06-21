5e09990515dcbe6afd01eb3d6f41c7a2b576ef6d
Tomas Mattia <tomas.mattia@gmail.com>
2022-06-20 21:26:37 -0700

    Refactor proxy and add http server

---
+++ http_server.rb
@@ -0,0 +1,42 @@
+require 'socket'
+
+class HTTPServer
+  def initialize(port)
+    @http_server = TCPServer.new(port)
+  end
+
+  def handle_request(session)
+    request = session.gets
+    puts request
+
+    session.print "HTTP/1.1 200\r\n" # 1
+    session.print "Content-Type: text/html\r\n" # 2
+    session.print "\r\n" # 3
+    session.print "Hello world! The time is #{Time.now}" #4
+
+    session.close
+  end
+
+  def run
+    loop do
+      session = @http_server.accept
+
+      Thread.new(session, &method(:handle_request))
+    end
+  end
+end
+
+if __FILE__ == $0
+  if ARGV.empty?
+    port = 80
+  elsif ARGV.size == 1
+    port = ARGV[0].to_i
+  else
+    puts 'Usage: http_server.rb [port]'
+    exit 1
+  end
+
+  puts "Starting HTTP server on port #{port}"
+  http_server = HTTPServer.new(port)
+  http_server.run
+end
--- proxy.rb
+++ proxy.rb
@@ -1,69 +1,30 @@
-#!/usr/bin/env ruby
-# A quick and dirty implementation of an HTTP proxy server in Ruby
-# because I did not want to install anything.
-#
-# Copyright (C) 2009-2014 Torsten Becker <torsten.becker@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 require 'socket'
 require 'uri'
 require 'openssl'
 
-
 class Proxy
-  def run(port)
-    begin
-      # Start our server to handle connections (will raise things on errors)
-      @socket = TCPServer.new port
-
-      # Handle every request in another thread
-      loop do
-        s = @socket.accept
-        Thread.new s, &method(:handle_request)
-      end
-
-      # CTRL-C
-    rescue Interrupt
-      puts 'Got Interrupt..'
-      # Ensure that we release the socket on errors
-    ensure
-      if @socket
-        @socket.close
-        puts 'Socked closed..'
-      end
-      puts 'Quitting.'
-    end
+  def initialize(port)
+    @socket = TCPServer.new(port)
   end
 
-  def handle_request to_client
-    request_line = to_client.readline
-    puts "Request line: #{request_line}"
+  def run
+    loop do
+      session = @socket.accept
+
+      Thread.new(session, &method(:handle_request))
+    end
+  ensure
+    @socket.close if @socket
+  end
+
+  def handle_request(client)
+    request_line = client.readline
+    puts request_line
 
     # Parse first request line
     verb    = request_line[/^\w+/]
     url     = request_line[/^\w+\s+(\S+)/, 1]
     version = request_line[/HTTP\/(1\.\d)\s*$/, 1]
-    puts "Verb: #{verb}"
-    puts "URL: #{url}"
-    puts "Version: #{version}"
 
     https = false
 
@@ -80,27 +41,18 @@ class Proxy
     else
       raise "Received unknown verb: #{verb}"
     end
-    puts "URI Host: #{uri.host}"
-    puts "URI Port: #{uri.port}"
-    puts "URI Path: #{uri.path}"
-    puts "URI Query: #{uri.query}"
 
-    # Show what got requested
-    puts((" %4s "%verb) + url)
-
-
-    # Open a socket to the destination server
     socket = TCPSocket.new(uri.host, (uri.port.nil? ? 80 : uri.port))
-
     if https
       ctx = OpenSSL::SSL::SSLContext.new
       ctx.set_params(verify_mode: OpenSSL::SSL::VERIFY_PEER)
-      to_server = OpenSSL::SSL::SSLSocket.new(socket, ctx) do |sock|
+      # TODO: this doesn't really work, figure out how to establish an SSL connection
+      server = OpenSSL::SSL::SSLSocket.new(socket, ctx) do |sock|
         sock.sync_close = true
         sock.connect
       end
     else
-      to_server = socket
+      server = socket
     end
 
     # Write the initial input to the destination server
@@ -109,13 +61,12 @@ class Proxy
     else
       payload = "#{verb} #{url} HTTP/#{version}\r\n"
     end
-    puts "Writing payload to server: #{payload}"
-    to_server.write(payload)
+    server.write(payload)
 
     content_len = 0
 
     loop do
-      line = to_client.readline
+      line = client.readline
 
       # Extract content length from client request
       if line =~ /^Content-Length:\s+(\d+)\s*$/
@@ -126,45 +77,47 @@ class Proxy
       if line =~ /^proxy/i
         next
       elsif line.strip.empty?
-        to_server.write("Connection: close\r\n\r\n")
+        server.write("Connection: close\r\n\r\n")
 
         if content_len >= 0
           # Read the remaining client payload and write it to the server
-          to_server.write(to_client.read(content_len))
+          server.write(client.read(content_len))
         end
 
         break
       else
-        to_server.write(line)
+        server.write(line)
       end
     end
 
     buff = ""
     loop do
       # Read response from server
-      to_server.read(4048, buff)
+      server.read(4048, buff)
 
       # Write response back to client
-      to_client.write(buff)
+      client.write(buff)
       break if buff.size < 4048
     end
 
     # Close the sockets
-    to_client.close
-    to_server.close
+    client.close
+    server.close
   end
 end
 
 
-# Get parameters and start the server
-if ARGV.empty?
-  port = 8008
-elsif ARGV.size == 1
-  port = ARGV[0].to_i
-else
-  puts 'Usage: proxy.rb [port]'
-  exit 1
-end
+if __FILE__ == $0
+  if ARGV.empty?
+    port = 8080
+  elsif ARGV.size == 1
+    port = ARGV[0].to_i
+  else
+    puts 'Usage: proxy.rb [port]'
+    exit 1
+  end
 
-puts "Starting proxy on port #{port}"
-Proxy.new.run port
+  puts "Starting proxy on port #{port}"
+  proxy = Proxy.new(port)
+  proxy.run
+end
